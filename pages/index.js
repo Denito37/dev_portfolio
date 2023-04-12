658c88b7de4d2bcf675faec53a8c21cc0228be0d
Denito37 <69406178+Denito37@users.noreply.github.com>
2023-04-12 07:56:01 -0400

    first build

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,16 @@ import Hero from '../src/components/hero'
 import Projects from '../src/components/projects.jsx'
 import Skills from '../src/components/skills.jsx'
 import Footer from '../src/components/footer'
+import projectData from '../src/components/dat/projects.json'
+import skillsData from '../src/components/dat/skills.json'
 
-export default function Home({ data, dataTwo }) {
-  const projects = data.map(project =>{
+export default function Home() {
+  const projects = projectData.map(project =>{
     return <Projects key={project.id} entry={project} />
   })
-  const skills = dataTwo.map( skill => {
-    return <Skills key={skill.id} entry={skill} />})
+  const skills = skillsData.map( skill => {
+    return <Skills key={skill.id} entry={skill} />
+  })
 
   return (
     <main className='grid place-content-center'>
@@ -28,13 +31,4 @@ export default function Home({ data, dataTwo }) {
         <Footer /> 
     </main>
   )
-}
-export async function getStaticProps(){
-    const res = await fetch ('http://localhost:3000/api/hello')
-    const data = await res.json()
-    const resTwo = await fetch ('http://localhost:3000/api/skills')
-    const dataTwo = await resTwo.json()
-    return { 
-      props: { data, dataTwo} 
-    };
 }
